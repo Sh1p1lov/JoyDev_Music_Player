5933324410e79419d5fc562ddc54282951a89eb1
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-01-09 05:18:10 +0300

    Added empty classes fragments and modified nav graph and modified activity_main.xml

---
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/fragments/SplashFragment.kt
@@ -0,0 +1,6 @@
+package com.sh1p1lov.joydevmusicplayer.fragments
+
+import androidx.fragment.app.Fragment
+import com.sh1p1lov.joydevmusicplayer.R
+
+class SplashFragment: Fragment(R.layout.fragment_splash)
