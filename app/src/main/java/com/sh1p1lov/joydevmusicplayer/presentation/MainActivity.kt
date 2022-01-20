c7861d0eac04456cab0784d87c7d87427be4f28e
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-01-20 07:02:34 +0300

    Added modules

--- app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/MainActivity.kt
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.sh1p1lov.joydevmusicplayer
+package com.sh1p1lov.joydevmusicplayer.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sh1p1lov.joydevmusicplayer.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
