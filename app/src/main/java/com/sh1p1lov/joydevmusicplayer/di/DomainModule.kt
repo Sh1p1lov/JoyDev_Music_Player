3bdf0a04fc4f03f8a67cb715298208d89e03d767
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-01-21 18:45:02 +0300

    Add constants

--- app/src/main/java/com/sh1p1lov/joydevmusicplayer/di/DomainModule.kt
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/di/DomainModule.kt
@@ -9,18 +9,37 @@ import com.sh1p1lov.joydevmusicplayer.domain.validator.LoginInputFieldValidator
 import com.sh1p1lov.joydevmusicplayer.domain.validator.PasswordInputFieldValidator
 import org.koin.dsl.module
 
+private const val MIN_LOGIN_TEXT_LENGTH = 6
+private const val MAX_LOGIN_TEXT_LENGTH = 20
+private const val MIN_EMAIL_TEXT_LENGTH = 6
+private const val MAX_EMAIL_TEXT_LENGTH = 256
+private const val MIN_PASSWORD_TEXT_LENGTH = 6
+private const val MAX_PASSWORD_TEXT_LENGTH = 20
+
 val domainModule = module {
 
     factory {
-        ValidateLoginInputFieldUseCase(LoginInputFieldValidator(6, 20))
+        ValidateLoginInputFieldUseCase(
+            LoginInputFieldValidator(
+            minTextLength = MIN_LOGIN_TEXT_LENGTH,
+            maxTextLength = MAX_LOGIN_TEXT_LENGTH
+        ))
     }
 
     factory {
-        ValidateEmailInputFieldUseCase(EmailInputFieldValidator(6, 254))
+        ValidateEmailInputFieldUseCase(
+            EmailInputFieldValidator(
+                minTextLength = MIN_EMAIL_TEXT_LENGTH,
+                maxTextLength = MAX_EMAIL_TEXT_LENGTH
+            ))
     }
 
     factory {
-        ValidatePasswordInputFieldUseCase(PasswordInputFieldValidator(6, 20))
+        ValidatePasswordInputFieldUseCase(
+            PasswordInputFieldValidator(
+                minTextLength = MIN_PASSWORD_TEXT_LENGTH,
+                maxTextLength = MAX_PASSWORD_TEXT_LENGTH
+            ))
     }
 
     factory {
