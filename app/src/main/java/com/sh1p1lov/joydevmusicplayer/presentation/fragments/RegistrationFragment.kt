4052983ddd8cf2d1193a42dfa725d57f529c91fc
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-01-21 16:41:19 +0300

    Modified RegistrationFragment.kt

--- app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/RegistrationFragment.kt
@@ -1,6 +1,30 @@
 package com.sh1p1lov.joydevmusicplayer.presentation.fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.sh1p1lov.joydevmusicplayer.R
+import com.sh1p1lov.joydevmusicplayer.databinding.FragmentRegistrationBinding
+import com.sh1p1lov.joydevmusicplayer.presentation.viewmodels.RegistrationViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class RegistrationFragment: Fragment(R.layout.fragment_registration)
+class RegistrationFragment : Fragment(R.layout.fragment_registration) {
+
+    private lateinit var binding: FragmentRegistrationBinding
+    private val vm by viewModel<RegistrationViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentRegistrationBinding.bind(view)
+
+        binding.registrationLoginInputField.doAfterTextChanged {
+            try {
+                vm.validateLogin(it.toString())
+            } catch (e: Exception) {
+                binding.registrationLoginInputLayout.error = e.message
+            }
+        }
+    }
+}
