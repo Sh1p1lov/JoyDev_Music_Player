7dd51be24dd5a046aa169797c444c12bde7dcda4
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-02-28 23:15:07 +0300

    added navigation from login fragment to registration fragment

--- app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/LoginFragment.kt
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/LoginFragment.kt
@@ -1,6 +1,23 @@
 package com.sh1p1lov.joydevmusicplayer.presentation.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.sh1p1lov.joydevmusicplayer.R
+import com.sh1p1lov.joydevmusicplayer.databinding.FragmentLoginBinding
 
-class LoginFragment: Fragment(R.layout.fragment_login)
+class LoginFragment: Fragment(R.layout.fragment_login) {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentLoginBinding.bind(view)
+
+        binding.loginRegistrationHyperlink.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+        }
+    }
+}
