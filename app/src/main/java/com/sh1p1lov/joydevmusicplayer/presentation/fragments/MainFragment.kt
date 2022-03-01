99b8c4c593e12bc1146e24e75c9ac13717af0758
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-03-01 11:52:42 +0300

    custom back action in main fragment

--- app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/MainFragment.kt
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/MainFragment.kt
@@ -1,6 +1,18 @@
 package com.sh1p1lov.joydevmusicplayer.presentation.fragments
 
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.sh1p1lov.joydevmusicplayer.R
 
-class MainFragment: Fragment(R.layout.fragment_main)
+class MainFragment: Fragment(R.layout.fragment_main) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
+    }
+}
