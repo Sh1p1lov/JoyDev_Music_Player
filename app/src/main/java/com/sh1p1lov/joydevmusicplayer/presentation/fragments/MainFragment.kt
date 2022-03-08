f14b7694d55714cb4b291c5734393ae904a281e8
Sh1p1lov <sergey.shipilov1999@mail.ru>
2022-03-08 11:13:14 +0300

    modified MainFragment.kt

--- app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/MainFragment.kt
+++ app/src/main/java/com/sh1p1lov/joydevmusicplayer/presentation/fragments/MainFragment.kt
@@ -1,7 +1,5 @@
 package com.sh1p1lov.joydevmusicplayer.presentation.fragments
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
@@ -13,85 +11,85 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.sh1p1lov.joydevmusicplayer.R
 import com.sh1p1lov.joydevmusicplayer.databinding.FragmentMainBinding
+import com.sh1p1lov.joydevmusicplayer.models.MusicTrackInfo
+import com.squareup.picasso.Picasso
 import java.lang.Exception
 
-private const val EXAMPLE_URL = "https://www.ssaurel.com/tmp/mymusic.mp3"
 private const val MEDIA_PLAYER_SEEK_TO_VALUE = 5000
+private const val BUNDLE_MUSIC_TRACK_KEY = "MUSIC_TRACK"
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private lateinit var binding: FragmentMainBinding
-    private val sharedPrefs: SharedPreferences by lazy {
-        requireActivity().getSharedPreferences(
-            getString(R.string.shared_prefs_name),
-            Context.MODE_PRIVATE
-        )
-    }
     private var mp: MediaPlayer? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentMainBinding.bind(view)
-        binding.mainCurrentUsername.text = sharedPrefs.getString(getString(R.string.current_username_key), "username")
-        binding.mainUrlInputField.setText(EXAMPLE_URL)
 
-        binding.mainPlayFromUrlButton.setOnClickListener {
-            mp?.release()
+        val musicTrackInfo = arguments?.getSerializable(BUNDLE_MUSIC_TRACK_KEY) as MusicTrackInfo
 
-            val url = binding.mainUrlInputField.text.toString()
-            mp = MediaPlayer().apply {
-                setAudioAttributes(
-                    AudioAttributes.Builder()
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                        .setUsage(AudioAttributes.USAGE_MEDIA)
-                        .build()
-                )
-            }
+        val imageView = binding.mainImageView
+        Picasso.with(context)
+            .load(musicTrackInfo.image)
+            .into(imageView)
 
-            try {
-                mp?.setDataSource(url)
-                mp?.prepareAsync()
-            }catch (e: Exception) {
-                val toast = Toast.makeText(context, "Ошибка воспроизведения", Toast.LENGTH_SHORT)
-                toast.show()
-                mp?.release()
-                mp = null
-            }
+        println(mp.toString())
+        mp?.release()
 
-            mp?.setOnPreparedListener {
-                it.start()
-                binding.mainMediaPlayButton.setImageResource(R.drawable.ic_baseline_pause_32)
-                val handler = Handler()
-                handler.postDelayed(object : Runnable {
-                    override fun run() {
-                        try {
-                            binding.mainSeekBar.progress = it.currentPosition / (it.duration / 100)
-                            binding.mainStartTime.text = millisecondsToNormalFormat(it.currentPosition)
-                            handler.postDelayed(this, 1000)
-                        } catch (e: Exception) {
-                            binding.mainSeekBar.progress = 0
-                        }
-                    }
-                }, 0)
-
-                binding.mainEndTime.text = millisecondsToNormalFormat(it.duration)
-            }
-
-            val errorListener = MediaPlayer.OnErrorListener { mp, what, extra ->
-                val toast = Toast.makeText(context, "Ошибка воспроизведения", Toast.LENGTH_SHORT)
-                toast.show()
-                this.mp?.release()
-                this.mp = null
-                true
-            }
-            mp?.setOnErrorListener(errorListener)
-
-            val bufferingUpdateListener = MediaPlayer.OnBufferingUpdateListener { mp, percent ->
-                binding.mainSeekBar.secondaryProgress = percent
-            }
-            mp?.setOnBufferingUpdateListener(bufferingUpdateListener)
+        val url = musicTrackInfo.audio
+        mp = MediaPlayer().apply {
+            setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
         }
+
+        try {
+            mp?.setDataSource(url)
+            mp?.prepareAsync()
+        }catch (e: Exception) {
+            val toast = Toast.makeText(context, "Ошибка воспроизведения", Toast.LENGTH_SHORT)
+            toast.show()
+            mp?.release()
+            mp = null
+        }
+
+        mp?.setOnPreparedListener {
+            it.start()
+            binding.mainMediaPlayButton.setImageResource(R.drawable.ic_baseline_pause_32)
+            val handler = Handler()
+            handler.postDelayed(object : Runnable {
+                override fun run() {
+                    try {
+                        binding.mainSeekBar.progress = it.currentPosition / (it.duration / 100)
+                        binding.mainStartTime.text = millisecondsToNormalFormat(it.currentPosition)
+                        handler.postDelayed(this, 1000)
+                    } catch (e: Exception) {
+                        binding.mainSeekBar.progress = 0
+                    }
+                }
+            }, 0)
+
+            binding.mainEndTime.text = millisecondsToNormalFormat(it.duration)
+        }
+
+        val errorListener = MediaPlayer.OnErrorListener { mp, what, extra ->
+            val toast = Toast.makeText(context, "Ошибка воспроизведения", Toast.LENGTH_SHORT)
+            toast.show()
+            this.mp?.release()
+            this.mp = null
+            true
+        }
+        mp?.setOnErrorListener(errorListener)
+
+        val bufferingUpdateListener = MediaPlayer.OnBufferingUpdateListener { mp, percent ->
+            binding.mainSeekBar.secondaryProgress = percent
+        }
+        mp?.setOnBufferingUpdateListener(bufferingUpdateListener)
 
         binding.mainMediaRewButton.setOnClickListener {
             mp?.seekTo(mp!!.currentPosition - MEDIA_PLAYER_SEEK_TO_VALUE)
@@ -117,22 +115,19 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             mp?.seekTo(mp!!.currentPosition + MEDIA_PLAYER_SEEK_TO_VALUE)
         }
 
-        binding.mainExitToAccountButton.setOnClickListener {
+        binding.mainExitToMusicList.setOnClickListener {
             mp?.release()
             mp = null
 
-            sharedPrefs
-                .edit()
-                .putBoolean(getString(R.string.login_status_key), false)
-                .remove(getString(R.string.current_username_key))
-                .apply()
-
-            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+            findNavController().navigate(R.id.action_mainFragment_to_musicListFragment)
         }
 
-        requireActivity().onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                requireActivity().finish()
+                mp?.release()
+                mp = null
+
+                findNavController().navigate(R.id.action_mainFragment_to_musicListFragment)
             }
         })
     }
